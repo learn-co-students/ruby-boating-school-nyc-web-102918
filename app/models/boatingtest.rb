784793dbe8dc8d7e8d672de076a29126fe5d4244
GermainDuran <arturodurantorres@gmail.com>
2018-11-03 18:10:02 -0400

    done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,22 @@
 class BoatingTest
+attr_accessor :student, :instructor, :boating_test_name, :boating_test_status
+@@all = []
+
+  #should initialize with student (Object), a boating test name (String),
+  #a boating test status (String), and an Instructor (Object)
+
+  def initialize(student,boating_test_name,boating_test_status,instructor)
+    @student = student
+    @boating_test_name = boating_test_name
+    @boating_test_status = boating_test_status
+    @instructor = instructor
+    @@all << self
+  end
+
+
+  #BoatingTest.all returns an array of all boating tests
+  def self.all
+    @@all
+  end
 
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,64 @@
 class Instructor
+attr_accessor :name
+@@all = []
 
-end
+#init with name
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  #should take in a student name and test name and return status passed
+  def self.pass_student(student_name,test_name)
+      student = Student.find_student student_name
+
+      boating_test = BoatingTest.all.find do |boating|
+                        boating.boating_test_name == test_name && boating.student == student
+                    end
+
+      boating_test.status = 'passed'
+  end
+
+
+  #Instructor.fail_student should take in a student name and test name and return status failed
+  def self.fail_student(student_name,test_name)
+      student = Student.find_student student_name
+
+      boating_test = BoatingTest.all.find do |boating|
+                        boating.boating_test_name == test_name && boating.student == student
+                    end
+
+      boating_test.status = 'failed'
+
+  end
+
+  #Instructor.student_grade_percentage should take in a student first name and output the
+  #percentage of tests that they have passed
+  def self.student_grade_percentage(first_name)
+      student = Student.find_student first_name
+      students_tests = BoatingTest.all.select do |test|
+        test.student == student
+      end
+
+      passed = students_tests.select do |test|
+        test.status == 'passed'
+      end.length
+
+      return passed / students_tests.length.to_f
+
+  end
+
+
+
+
+
+
+
+
+
+end #end of the class
