56d6b4628c286061174f9433a3ed6916673cc9fa
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-03 16:38:14 -0400

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,25 @@
 class Student
+attr_accessor :first_name
 
+def initialize(first_name)
+  @first_name=first_name
+end
+
+def self.all
+  BoatingTest.all.map{|boating_test|boating_test.student}.uniq
+end
+
+# def self.all
+#   @@all
+# end
+
+def add_boating_test(test_name,status,instructor)
+  BoatingTest.new(self,test_name,status,instructor)
+end
+
+
+def self.find_student(name)
+  test_instance=BoatingTest.all.find{|boating_test|boating_test.student.first_name==name}
+  test_instace.student
+end
 end
