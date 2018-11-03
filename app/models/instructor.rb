fb27e87602eb56e2c9400a8b43e6b8cca2222217
gavinmoore36 <gavizoid16@gmail.com>
2018-11-03 16:52:18 -0400

    done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,33 @@
 class Instructor
-
+  attr_accessor :name, :student, :boatingtest
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.pass_student(student, quiz)
+    selected_test = BoatingTest.all.find do |person|
+    person.student == student && person.test_name == quiz
+  end
+    selected_test.test_status = "passed"
+  end
+  def self.fail_student(student_name, quiz)
+    selected_test = BoatingTest.all.find do |tests|
+    tests.student == student_name && tests.test_name == quiz
+  end
+    selected_test.test_status = "failed"
+  end
+  def self.student_grade_percentage(student)
+    passed_test = BoatingTest.all.select do |tests|
+      tests.student == student && tests.test_status == "passed"
+    end
+    all_student_tests = BoatingTest.all.select do |tests|
+      tests.student == student
+    end
+    percentage = (passed_test.length/all_student_tests.length.to_f)*100
+    "#{percentage}%"
+  end
+  def self.all
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,17 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+spongebob= Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+test1= spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+test2= spongebob.add_boating_test("Don't Smoke and Drive", "pending", puff)
+test3= patrick.add_boating_test("Don't Crash 101", "pending", puff)
+passed1 = Instructor.pass_student(spongebob, "Don't Crash 101")
+failed1 = Instructor.fail_student(spongebob, "Don't Smoke and Drive")
+percentage1 = Instructor.student_grade_percentage(spongebob)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
