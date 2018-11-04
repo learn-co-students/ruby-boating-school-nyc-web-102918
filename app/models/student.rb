a7efc4e2aeaef5864fab4c19eb1fb9f7d2cda8c4
digitopolis <matt.readout@gmail.com>
2018-11-03 23:09:50 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(student_name)
+    self.all.find do |student|
+      student.first_name == student_name
+    end
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    new_test = BoatingTest.new(self, test_name, test_status, instructor)
+    new_test
+  end
 end
