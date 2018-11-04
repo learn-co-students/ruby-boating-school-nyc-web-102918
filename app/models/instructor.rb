72c418ff8da44171aa732c881499b71a225f4fff
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-11-04 18:04:27 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,55 @@
 class Instructor
 
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.pass_student(student_name, testName)
+    answerTest = BoatingTest.all.find do |each_test|
+      each_test.student.first_name == student_name && each_test.test_name == testName
+    end
+    answerTest.test_status = "passed"
+    "status passed"
+  end
+
+  def self.fail_student(student_name, testName)
+    answerTest = BoatingTest.all.find do |each_test|
+      each_test.student.first_name == student_name && each_test.test_name == testName
+    end
+    answerTest.test_status = "failed"
+    "status failed"
+  end
+
+  def self.student_grade_percentage(student_name)
+    my_test_array = BoatingTest.all.select do |each_test|
+      each_test.student.first_name == student_name
+    end
+    my_test_array
+    pass_counter = 0
+    pass_percent = 0
+    my_test_array.each do |my_test|
+      if my_test.test_status == "passed"
+        pass_counter += 1
+      end
+    end
+    pass_percent = pass_counter.to_f/my_test_array.length.to_f
+    final = pass_percent.to_f*100
+    return "%#{final}"
+
+    end
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+student1 = Student.new ("vibhu")
+student2 =  Student.new ("Nirma")
+
+
+
+
+instructor1 = Instructor.new ("swami sir")
+
+
+
+
+boatingtest2 = BoatingTest.new(student1,"lesson one","pending",instructor1)
+student1.add_boating_test("lesson three","pending",instructor1)
+boatingtest1 = BoatingTest.new(student2,"lesson two","pending",instructor1)
+
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
