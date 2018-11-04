253601f3d76e46828510e09b1e80f39f8037d488
github username <github email address>
2018-11-04 13:00:50 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
 
+  attr_accessor :first_name, :tests
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(first_name)
+    Student.all.find do |student|
+      student.first_name == first_name
+    end
+  end
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def tests
+    BoatingTest.all.select do |test|
+      test.student == self
+    end
+  end
 end
